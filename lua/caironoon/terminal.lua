5dd6afd1a44d54247755bad924294aefc5a8ce41
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-03-21 16:49:18 +0200

    Use 'Open Color' color names, and set 'comment' color in the palette.

--- lua/caironoon/terminal.lua
+++ lua/caironoon/terminal.lua
@@ -4,20 +4,22 @@ local p = require 'caironoon.palette'
 
 function M.setup()
     if not cfg.term_colors then return end
+
     vim.g.terminal_color_0 = p.black
     vim.g.terminal_color_1 = p.red
     vim.g.terminal_color_2 = p.green
     vim.g.terminal_color_3 = p.yellow
     vim.g.terminal_color_4 = p.blue
-    vim.g.terminal_color_5 = p.purple
+    vim.g.terminal_color_5 = p.grape
     vim.g.terminal_color_6 = p.cyan
     vim.g.terminal_color_7 = p.fg
-    vim.g.terminal_color_8 = p.grey
+
+    vim.g.terminal_color_8 = p.gray
     vim.g.terminal_color_9 = p.red
     vim.g.terminal_color_10 = p.green
     vim.g.terminal_color_11 = p.yellow
     vim.g.terminal_color_12 = p.blue
-    vim.g.terminal_color_13 = p.purple
+    vim.g.terminal_color_13 = p.grape
     vim.g.terminal_color_14 = p.cyan
     vim.g.terminal_color_15 = p.fg
 end
