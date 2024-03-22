cca2ef8ca4e4f32fe45975fa7516f61ce32fcb2d
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-03-22 14:03:38 +0200

    Revert "Use 'Open Color' color names, and set 'comment' color in the palette."
    This reverts commit 5dd6afd1a44d54247755bad924294aefc5a8ce41.

--- lua/caironoon/palette.lua
+++ lua/caironoon/palette.lua
@@ -2,17 +2,16 @@ local oc = require('caironoon.open-color')
 
 local dark = {
 	black       = oc.black,
-	white       = oc.white,
-
-	bg0         = oc.black,
+	bg0         = oc.gray[10],
 	bg1         = oc.gray[9],
 	bg2         = oc.gray[8],
 	bg3         = oc.gray[7],
 	bg_d        = oc.black,
 	bg_blue     = oc.blue[5],
-	fg          = oc.gray[4],
+	bg_yellow   = oc.yellow[2],
+	fg          = oc.gray[5],
 
-	gray        = oc.gray[7],
+	grey        = oc.gray[7],
 	red         = oc.red[4],
 	orange      = oc.orange[4],
 	yellow      = oc.yellow[4],
@@ -23,10 +22,10 @@ local dark = {
 	blue        = oc.blue[4],
 	indigo      = oc.indigo[4],
 	violet      = oc.violet[4],
-	grape       = oc.grape[4],
+	purple      = oc.grape[4],
 	pink        = oc.pink[4],
 
-	light_gray  = oc.gray[6],
+	light_grey  = oc.gray[6],
 	dark_cyan   = oc.cyan[10],
 	dark_red    = oc.red[10],
 	dark_yellow = oc.yellow[10],
@@ -36,22 +35,19 @@ local dark = {
 	diff_delete = oc.red[10],
 	diff_change = oc.blue[10],
 	diff_text   = oc.gray[9],
-
-	comment     = oc.gray[7],
 }
 local light = {
 	black       = oc.black,
-	white       = oc.white,
-
-	bg0         = oc.white,
+	bg0         = oc.gray[1],
 	bg1         = oc.gray[3],
 	bg2         = oc.gray[4],
 	bg3         = oc.gray[5],
 	bg_d        = oc.gray[6],
 	bg_blue     = oc.blue[4],
+	bg_yellow   = oc.yellow[5],
 	fg          = oc.black,
 
-	gray        = oc.gray[8],
+	grey        = oc.gray[8],
 	red         = oc.red[10],
 	orange      = oc.orange[10],
 	yellow      = oc.yellow[10],
@@ -62,10 +58,10 @@ local light = {
 	blue        = oc.blue[10],
 	indigo      = oc.indigo[10],
 	violet      = oc.violet[10],
-	grape      = oc.grape[10],
+	purple      = oc.grape[10],
 	pink        = oc.pink[10],
 
-	light_gray  = oc.gray[7],
+	light_grey  = oc.gray[7],
 	dark_cyan   = oc.cyan[10],
 	dark_red    = oc.red[10],
 	dark_yellow = oc.yellow[10],
@@ -75,8 +71,6 @@ local light = {
 	diff_delete = oc.red[2],
 	diff_change = oc.blue[2],
 	diff_text   = oc.gray[5],
-
-	comment     = oc.gray[8],
 }
 
 if vim.opt.background:get() == 'dark' then
