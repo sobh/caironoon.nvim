12fb4ea3e87a85d4580fae528ae9938db8eaf5b2
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-03-23 14:56:51 +0200

    Dropped user color overrides.

--- lua/caironoon/highlights.lua
+++ lua/caironoon/highlights.lua
@@ -727,41 +727,6 @@ function M.setup()
 	for _, group in pairs(groups.plugins) do
 		vim_highlights(group)
 	end
-
-	-- user defined highlights: vim_highlights function cannot be used because it sets an attribute to none if not specified
-	local function replace_color(prefix, color_name)
-		if not color_name then
-			return ""
-		end
-		if color_name:sub(1, 1) == "$" then
-			local name = color_name:sub(2, -1)
-			color_name = p[name]
-			if not color_name then
-				vim.schedule(function()
-					vim.notify(
-						'caironoon.nvim: unknown color "' .. name .. '"',
-						vim.log.levels.ERROR,
-						{ title = "caironoon.nvim" }
-					)
-				end)
-				return ""
-			end
-		end
-		return prefix .. "=" .. color_name
-	end
-
-	for group_name, group_settings in pairs(vim.g.caironoon_config.highlights) do
-		vim.api.nvim_command(
-			string.format(
-				"highlight %s %s %s %s %s",
-				group_name,
-				replace_color("guifg", group_settings.fg),
-				replace_color("guibg", group_settings.bg),
-				replace_color("guisp", group_settings.sp),
-				replace_color("gui", group_settings.fmt)
-			)
-		)
-	end
 end
 
 return M
