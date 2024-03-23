91e089feb110272c9eb68aac23b6abdd736c9480
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-03-23 20:56:39 +0200

    Code reformat.

--- lua/caironoon/init.lua
+++ lua/caironoon/init.lua
@@ -5,71 +5,71 @@ local M = {}
 ---@param opt string: option name
 ---@param value any: new value
 function M.set_options(opt, value)
-    local cfg = vim.g.caironoon_config
-    cfg[opt] = value
-    vim.g.caironoon_config = cfg
+	local cfg = vim.g.caironoon_config
+	cfg[opt] = value
+	vim.g.caironoon_config = cfg
 end
 
 ---Apply the colorscheme (same as ':colorscheme caironoon')
 function M.colorscheme()
-    vim.cmd("hi clear")
-    if vim.fn.exists("syntax_on") then
-	    vim.cmd("syntax reset")
-    end
-    vim.o.termguicolors = true
-    vim.g.colors_name = "caironoon"
-    require('caironoon.highlights').setup()
-    require('caironoon.terminal').setup()
+	vim.cmd("hi clear")
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
+	end
+	vim.o.termguicolors = true
+	vim.g.colors_name = "caironoon"
+	require("caironoon.highlights").setup()
+	require("caironoon.terminal").setup()
 end
 
 local default_config = {
-    -- Main options --
-    style = 'light',    -- choose between 'dark', 'light'
-    transparent = false,     -- don't set background
-    term_colors = true,      -- if true enable the terminal
-    ending_tildes = false,    -- show the end-of-buffer tildes
-    cmp_itemkind_reverse = false,    -- reverse item kind highlights in cmp menu
+	-- Main options --
+	style = "light", -- choose between 'dark', 'light'
+	transparent = false, -- don't set background
+	term_colors = true, -- if true enable the terminal
+	ending_tildes = false, -- show the end-of-buffer tildes
+	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
-    -- Changing Formats --
-    code_style = {
-        comments  = 'none',
-        keywords  = 'italic',
-        functions = 'bold',
-        strings   = 'none',
-        variables = 'none'
-    },
+	-- Changing Formats --
+	code_style = {
+		comments = "none",
+		keywords = "italic",
+		functions = "bold",
+		strings = "none",
+		variables = "none",
+	},
 
-    -- Lualine options --
-    lualine = {
-        transparent = false, -- center bar (c) transparency
-    },
+	-- Lualine options --
+	lualine = {
+		transparent = false, -- center bar (c) transparency
+	},
 
-    -- Custom Highlights --
-    colors = {}, -- Override default colors
-    highlights = {}, -- Override highlight groups
+	-- Custom Highlights --
+	colors = {}, -- Override default colors
+	highlights = {}, -- Override highlight groups
 
-    -- Plugins Related --
-    diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true,   -- use undercurl for diagnostics
-        background = true,    -- use background color for virtual text
-    },
+	-- Plugins Related --
+	diagnostics = {
+		darker = true, -- darker colors for diagnostic
+		undercurl = true, -- use undercurl for diagnostics
+		background = true, -- use background color for virtual text
+	},
 }
 
 ---Setup caironoon.nvim options, without applying colorscheme
 ---@param opts table: a table containing options
 function M.setup(opts)
-    if not vim.g.caironoon_config or not vim.g.caironoon_config.loaded then    -- if it's the first time setup() is called
-        vim.g.caironoon_config = vim.tbl_deep_extend('keep', vim.g.caironoon_config or {}, default_config)
-        M.set_options('loaded', true)
-    end
-    if opts then
-        vim.g.caironoon_config = vim.tbl_deep_extend('force', vim.g.caironoon_config, opts)
-    end
+	if not vim.g.caironoon_config or not vim.g.caironoon_config.loaded then -- if it's the first time setup() is called
+		vim.g.caironoon_config = vim.tbl_deep_extend("keep", vim.g.caironoon_config or {}, default_config)
+		M.set_options("loaded", true)
+	end
+	if opts then
+		vim.g.caironoon_config = vim.tbl_deep_extend("force", vim.g.caironoon_config, opts)
+	end
 end
 
 function M.load()
-  vim.api.nvim_command('colorscheme caironoon')
+	vim.api.nvim_command("colorscheme caironoon")
 end
 
 return M
